d4b473da6eb0b6a743144ea74b8d20ce94354cbe
zhangddjs <1005155946@qq.com>
2019-12-26 18:24:24 +0800

    portal and item done. changed some port config and mysql db.properties

---
+++ ego-item/src/main/java/com/ego/item/controller/TbItemCatController.java
@@ -0,0 +1,29 @@
+package com.ego.item.controller;
+
+import com.ego.item.service.TbItemCatService;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+public class TbItemCatController {
+    @Resource
+    private TbItemCatService tbItemCatServiceImpl;
+
+    /**
+     * 返回jsonp数据格式包含所有菜单信息
+     * @param callback
+     * @return
+     */
+    @RequestMapping("rest/itemcat/all")
+    @ResponseBody
+    public MappingJacksonValue showMenu(String callback){
+        MappingJacksonValue mjv = new MappingJacksonValue(tbItemCatServiceImpl.showCatMenu());
+        mjv.setJsonpFunction(callback);
+        //返回会多一个/**/
+        return mjv;
+    }
+}
---
+++ ego-item/src/main/java/com/ego/item/service/TbItemCatService.java
@@ -0,0 +1,11 @@
+package com.ego.item.service;
+
+import com.ego.item.pojo.PortalMenu;
+
+public interface TbItemCatService {
+    /**
+     * 查询所有分类类目并转换为特定类型
+     * @return
+     */
+    PortalMenu showCatMenu();
+}
