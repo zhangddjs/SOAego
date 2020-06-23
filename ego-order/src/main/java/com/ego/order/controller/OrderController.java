c589f2fee445843269ce0f4c43f7c8549f165b0f
Dongdong Zhang <dongdzha@cisco.com>
2020-06-23 23:52:05 +0800

    order

---
+++ ego-order/src/main/java/com/ego/order/controller/OrderController.java
@@ -0,0 +1,44 @@
+package com.ego.order.controller;
+
+import com.ego.commons.pojo.EgoResult;
+import com.ego.order.pojo.MyOrderParam;
+import com.ego.order.service.TbOrderService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class OrderController {
+    @Resource
+    private TbOrderService tbOrderServiceImpl;
+
+    /**
+     * 显示订单确认页面
+     * @param model
+     * @param ids
+     * @param request
+     * @return
+     */
+    @RequestMapping("order/order-cart.html")
+    public String showCartOrcer(Model model,@RequestParam("id") List<Long> ids, HttpServletRequest request){
+        model.addAttribute("cartList", tbOrderServiceImpl.showOrderCart(ids, request));
+        return "order-cart";
+    }
+
+    @RequestMapping("order/create.html")
+    public String createOrder(MyOrderParam param, HttpServletRequest request){
+        EgoResult er = tbOrderServiceImpl.create(param, request);
+        if(er.getStatus() == 200){
+            return "my-orders";
+        } else {
+            request.setAttribute("message", "订单创建失败");
+            return "error/exception";
+        }
+    }
+
+}
