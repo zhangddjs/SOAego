070095e6fd96f63657c5c003f861b34cabc80922
zhangddjs <1005155946@qq.com>
2020-07-06 20:34:04 +0800

    dubbo-service

--- ego-redis/src/test/java/com/ego/redis/dao/redisTest.java
+++ ego-redis/src/test/java/com/ego/redis/dao/redisTest.java
@@ -119,5 +119,94 @@ public class redisTest {
         shardedJedisPool.close();
     }
 
+    @Test
+    @Ignore
+    public void testt(){
+        Solution s = new Solution();
+        s.nthUglyNumber(1389);
+    }
 
+    class Solution {
+        public int nthUglyNumber(int n) {
+            if(n <= 6) return n;
+            int count = 6;
+            int Ugly = 6;
+            PreUgly preUgly = new PreUgly();
+            List<Integer> L1 = preUgly.L1;
+            List<Integer> L2 = preUgly.L2;
+            List<Integer> L3 = preUgly.L3;
+            List<List<Integer>> L = preUgly.L;
+            int[] i = preUgly.i;
+            int[] j = preUgly.j;
+            while(count++ != n){
+                if(count == 1388){
+                    System.out.println(1388);
+                }
+                Ugly = getMin(L1.get(L1.size() - 1), L2.get(L2.size() - 1), L3.get(L3.size() - 1));
+                if(L1.get(L1.size() - 1) == Ugly){
+                    int z1 = L1.get(i[0]) * 2;
+                    int z2 = L2.get(i[1]) * 2;
+                    int z3 = i[2] >= L3.size() ? Integer.MAX_VALUE - 1 : L3.get(i[2]) * 2;
+                    L1.add(getMin(z1, z2, z3));
+                    updateIndex(i, L, L1, 2);
+                } else if(L2.get(L2.size() - 1) == Ugly){
+                    int z1 = L2.get(j[0]) * 3;
+                    int z2 = L3.get(j[1]) * 3;
+                    L2.add(Math.min(z1, z2));
+                    updateIndex(j, L, L2, 3);
+                } else if(L3.get(L3.size() - 1) == Ugly){
+                    L3.add(L3.get(L3.size() - 1) * 5);
+                }
+            }
+            return Ugly;
+        }
+
+        public int getMin(int a, int b, int c){
+            return Math.min(a, Math.min(b, c));
+        }
+
+        public void updateIndex(int[] index, List<List<Integer>> L, List<Integer> obj, int factor){
+            int flag = index.length == 3 ? 0 : 1;
+            for(int i = 0; i < index.length; i++){
+                if(index[i] < L.get(i + flag).size()) index[i] = obj.get(obj.size() - 1) == L.get(i + flag).get(index[i]) * factor ? index[i] + 1 : index[i];
+            }
+        }
+
+        class PreUgly{
+            List<Integer> L1;
+            List<Integer> L2;
+            List<Integer> L3;
+            List<List<Integer>> L;
+            int [] i;
+            int [] j;
+            public PreUgly(){
+                L1 = new ArrayList<Integer>();
+                L2 = new ArrayList<Integer>();
+                L3 = new ArrayList<Integer>();
+                L = new ArrayList<List<Integer>>();
+                i = new int[3];
+                j = new int[2];
+                init();
+            }
+
+            public void init(){
+                L1.add(2);
+                L1.add(4);
+                L1.add(6);
+                L1.add(8);
+                L2.add(3);
+                L2.add(9);
+                L3.add(5);
+                L3.add(25);
+                L.add(L1);
+                L.add(L2);
+                L.add(L3);
+                i[0] = 2;
+                i[1] = 1;
+                i[2] = 0;
+                j[0] = 1;
+                j[1] = 0;
+            }
+        }
+    }
 }
