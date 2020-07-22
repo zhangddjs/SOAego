8fd9af361099baab7451c333002db041344c2269
Dongdong Zhang <dongdzha@cisco.com>
2020-07-23 00:26:03 +0800

    deploy

--- ego-order/src/main/java/com/ego/order/interceptor/LoginInterceptor.java
+++ ego-order/src/main/java/com/ego/order/interceptor/LoginInterceptor.java
@@ -22,7 +22,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 
         String token = CookieUtils.getCookieValue(httpServletRequest, "TT_TOKEN");
         if(token != null && !token.equals("")){
-            String json = HttpClientUtil.doPost("http://localhost:8075/user/token/" + token);
+            String json = HttpClientUtil.doPost("http://passport.ego.com/user/token/" + token);
             EgoResult er = JsonUtils.jsonToPojo(json, EgoResult.class);
             if(er.getStatus() == 200){
                 return true;
@@ -30,8 +30,8 @@ public class LoginInterceptor implements HandlerInterceptor {
         }
         String num = httpServletRequest.getParameter("num");
         if(num != null && !num.equals(""))
-            httpServletResponse.sendRedirect("http://localhost:8075/user/showLogin?interurl="+httpServletRequest.getRequestURL()+"%3Fnum=" + num);
-        else httpServletResponse.sendRedirect("http://localhost:8075/user/showLogin");
+            httpServletResponse.sendRedirect("http://passport.ego.com/user/showLogin?interurl="+httpServletRequest.getRequestURL()+"%3Fnum=" + num);
+        else httpServletResponse.sendRedirect("http://passport.ego.com/user/showLogin");
         return false;
     }
 
